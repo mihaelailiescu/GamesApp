3dae49e3ad9f9a916d591f8ac36c58060a18e6a3
mihaelailiescu <mihaela.iliescu1708@yahoo.com>
2020-03-23 20:05:04 +0200

    get list of games

---
+++ model.js
@@ -0,0 +1,12 @@
+var apiURL = "https://games-world.herokuapp.com";
+
+fetch(apiURL + "/games", {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+}).then(function(response) {
+    return response.json();
+}).then(function(jsonResponse) {
+    console.log("the response ", jsonResponse);
+});
